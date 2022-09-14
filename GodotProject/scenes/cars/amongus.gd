f4173185d5883806633c0d972056db326e757aac
e <43293694+mikoaj@users.noreply.github.com>
2022-09-14 18:28:41 +0200

    amongus
    e

---
+++ GodotProject/scenes/cars/amongus.gd
@@ -0,0 +1,17 @@
+extends VehicleBody
+var MAX_ENGINE_FORCE=700.0
+var MAX_BRAKE_FORCE= 50.0
+
+
+
+
+func _ready():
+	pass 
+
+func _physics_process(delta):
+	if(Input.is_action_pressed('ui_up')):
+		engine_force=MAX_ENGINE_FORCE 
+	else:
+		engine_force=0
+		
+		
